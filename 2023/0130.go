7f78e9132755c026f671aeadfe91bac6b43dfd03
李华峰2010 <lhf2008@yeah.net>
2023-01-30 09:51:04 +0800

    '每日一题';

---
+++ 2023/0130.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	/**
+	通过指针变量 p 访问其成员变量 name，有哪几种方式？（多选）
+
+	A.p.name
+	B.(&p).name
+	C.(*p).name
+	D.p->name
+
+	答案解析：
+	参考答案：AC
+
+	参考解析：& 取址运算符，* 指针解引用。
+	 */
+}
---
+++ 2023/lk0129-2.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
